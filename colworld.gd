607e575a43cf8e6f091c276835b0555e03a258b8
Watson <ripppony@gmail.com>
2019-07-06 15:45:39 +0100

    Experiment with changing backgroud colour

--- colworld.gd
+++ colworld.gd
@@ -11,8 +11,14 @@ onready var femaleWalls = get_node("FemaleWalls");
 func _input(event):
 	if event is InputEventKey and event.pressed and not event.echo:
 		if event.scancode == KEY_E:
-			baseWalls.enabled = !baseWalls.enabled
+			maleWalls.enabled = true;
+			femaleWalls.enabled = true;
+			VisualServer.set_default_clear_color(Color8(130,130,130))
 		if event.scancode == KEY_R:
-			maleWalls.enabled = !maleWalls.enabled
+			maleWalls.enabled = true;
+			femaleWalls.enabled = false;
+			VisualServer.set_default_clear_color(Color8(255,130,110))
 		if event.scancode == KEY_T:
-			femaleWalls.enabled = !femaleWalls.enabled
+			maleWalls.enabled = false;
+			femaleWalls.enabled = true;
+			VisualServer.set_default_clear_color(Color8(20,45,230))
