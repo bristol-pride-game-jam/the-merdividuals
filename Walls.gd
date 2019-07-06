7ed5fcbfca6fa446aacacff8620dd3566107bceb
Watson <ripppony@gmail.com>
2019-07-06 17:55:00 +0100

    add space to reset hats@

--- Walls.gd
+++ Walls.gd
@@ -2,19 +2,6 @@ extends TileMap
 
 class_name togglable_wall
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-
-
 var enabled setget set_enabled,get_enabled;
 
 func get_enabled():
--- colworld.gd
+++ colworld.gd
@@ -1,34 +1,30 @@
 extends Node2D
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+var default_color = ColorN("gray")
 
-onready var baseWalls = get_node("BaseWalls");
-onready var maleWalls = get_node("MaleWalls");
-onready var femaleWalls = get_node("FemaleWalls");
-
-var color setget set_color
-
+# Controls the background colour for the level
+export var color:Color = default_color setget set_color
 func set_color(value):
 	color=value
 	VisualServer.set_default_clear_color(value)
 
+func _ready():
+	set_color(color);
 
-func clearWall(wallName):
+# Enables all walls except the specified wall
+# If none are specified then none are disabled
+func clearWall(wallName=null):
 	for x in get_children():
 		if x is togglable_wall:
 			x.enabled = true
-	get_node(wallName).enabled = false;
+	if wallName != null:
+		get_node(wallName).enabled = false;
 
+# Spacebar - resets current hats
 func _input(event):
-	if event is InputEventKey and event.pressed and not event.echo:
-		if event.scancode == KEY_E:
-			maleWalls.enabled = true;
-			femaleWalls.enabled = true;
-		if event.scancode == KEY_R:
-			maleWalls.enabled = true;
-			femaleWalls.enabled = false;
-		if event.scancode == KEY_T:
-			maleWalls.enabled = false;
-			femaleWalls.enabled = true;
+	if (event is InputEventKey and
+		event.pressed and
+		not event.echo and
+		event.scancode == KEY_SPACE):
+			clearWall()
+			set_color(default_color)
