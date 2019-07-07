97da1fc2ec63a01bce5cfe20a15058a3b1c2c19f
Kit Manners <kitm@nne.rs>
2019-07-07 12:41:48 +0100

    Add new player animations

--- sprite.gd
+++ sprite.gd
@@ -8,7 +8,7 @@ extends Sprite
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$AnimationPlayer.play("player")
+	$AnimationPlayer.play("idle")
 	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
